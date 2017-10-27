df450090691c750a27968b16ab93c047151277f9
Alex <oleksandr.barabanov@gmail.com>
2017-10-27 17:40:12 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+/*
+function onLoad() {
+    console.log('DOM loaded');    
+}
+*/
+
+const picture = document.createElement("img"); 
+picture.src = 'http://csharpcorner.mindcrackerinc.netdna-cdn.com/article/round-image-in-xamarin-forms/Images/image005.jpg';
+
+
+function closePopup() {
+    const popup = document.getElementById('popup');    
+    popup.style.display = 'none';
+}
+
+function screenClicked(event) {
+    //console.log('click:', event);    
+
+    const popup = document.getElementById('popup');    
+    popup.style.display = 'unset';
+    
+    // clientY & clientX
+    const {y: top, x: left} = event;
+    console.log(`event.y=${event.y} x=${event.x}`);
+    //console.log(`event.target=${event.y} x=${event.x}`);
+    //renderImageAt(top, left);
+    
+}
+
+function renderImageAt(top, left) {
+
+    document.body.appendChild(picture);
+    
+    //  works - OK
+    //picture.style.position = 'absolute';
+    picture.style.top = `${top}px`;
+    picture.style.left = `${left}px`;
+
+    //console.log('style.position:', picture.style.position);
+    //console.log('style:', picture.style);
+}    
+
+function search() {
+    console.log('search');    
+    
+    let url;
+    //url = 'https://www.google.com.ua/search?q=puppy&tbm=isch'; // empty
+    //url = 'https://www.google.com.ua/search?q=puppy'; // empty
+    //url = 'https://www.google.com/search?q=puppy';
+    url = 'https://api.qwant.com/api/search/images?count=10&offset=1&q=cars';
+    const json = fetch(url, { mode: 'no-cors' });
+    json
+        .then(data => {
+            console.log( data );
+            //data.json()
+        })
+        .catch(err => console.error(err));
+    
+}
---
+++ todo.md
@@ -0,0 +1,24 @@
+
+# TODO
+## Ideas
+
+* put small cross-mark on clicked place
+* open a floating popup
+* find & select image
+* insert `<img/>` instead of cross-mark - use absolute positioning ?
+* have top-fixed options panel with 'show / hide image borders', and some others ?
+* option: **download** image firstly, then embed it from localhost ?
+
+## The task is to:
+
+* create initially an empty web page
+* when the user clicks on the screen a popup box appears
+* the box allows allows the user to search google for, and select an image
+* the image is then inserted into the webpage at the location of the click
+
+We are interested in your architecture, and code design/organisation, and approach. Choose any language you like.
+ 
+Bonus points for not using any external packages to get the images. 
+
+Its fine to make and document any assumptions you need to in order to complete the task. 
+When you are done please send us the link to the GitHub project where it resides.
