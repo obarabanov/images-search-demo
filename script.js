54f995af5f0bdf63c25bf3c346f1b074f04197bb
Alex <oleksandr.barabanov@gmail.com>
2017-10-30 18:14:23 +0200

    Core functionality implemented, using Google Custom Search.

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-/*
-function onLoad() {
-    console.log('DOM loaded');    
-}
-*/
 
+var position;
+/*
 const picture = document.createElement("img"); 
 picture.src = 'http://csharpcorner.mindcrackerinc.netdna-cdn.com/article/round-image-in-xamarin-forms/Images/image005.jpg';
+*/
+
+
+function onLoad() {
+    console.log('DOM loaded');    
+
+    const body = document.getElementsByTagName('body')[0];
+    body.onclick = function(event) {
+    //document.onclick = function(event) {
+        console.log('clicked:', event);
+
+        if ( event.target.classList.contains('gs-image') ) {
+            event.preventDefault();
+            console.log('img.src: ', event.target.src);
+
+            //  TODO:   
+            //  1. close search results
+            //  2. add image to screen
+
+            const { top, left } = position;           
+            renderImageAt(event.target.src, top, left);
+
+        }
+    }
+}
 
 
 function closePopup() {
@@ -23,18 +45,27 @@ function screenClicked(event) {
     const {y: top, x: left} = event;
     console.log(`event.y=${event.y} x=${event.x}`);
     //console.log(`event.target=${event.y} x=${event.x}`);
+
+    position = {
+        top,
+        left
+    };
     //renderImageAt(top, left);
-    
+    console.log('position:', position);    
 }
 
-function renderImageAt(top, left) {
+function renderImageAt(src, top, left) {
 
+    const picture = document.createElement("img"); 
+    picture.src = src;
+        
     document.body.appendChild(picture);
     
     //  works - OK
-    //picture.style.position = 'absolute';
+    picture.style.position = 'absolute';
     picture.style.top = `${top}px`;
     picture.style.left = `${left}px`;
+    picture.style.border = '1px solid red';
 
     //console.log('style.position:', picture.style.position);
     //console.log('style:', picture.style);
@@ -55,8 +86,12 @@ function search() {
     json
         .then(data => {
             console.log( data );
-            //data.json()
         })
+        /*
+        .then(json => {
+            console.log( json ); // undefined
+        })
+        */
         .catch(err => console.error(err));
     
 }
